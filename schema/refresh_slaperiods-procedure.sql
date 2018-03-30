bb70d223e464bc377a9e551657c6f121c61ebd6b
Thomas Gelf <thomas@gelf.net>
2018-03-30 14:32:05 +0200

    Add missing logic to period refresh

--- schema/refresh_slaperiods-procedure.sql
+++ schema/refresh_slaperiods-procedure.sql
@@ -25,9 +25,7 @@ BEGIN
   SET SESSION binlog_format = ROW;
 
   START TRANSACTION;
-
-  TRUNCATE TABLE icinga_sla_periods;
-   TRUNCATE TABLE icinga_outofsla_periods;
+  TRUNCATE TABLE icinga_outofsla_periods;
 
   SELECT
       CAST(DATE_FORMAT(DATE_SUB(NOW(), INTERVAL 5 YEAR), '%Y-01-01 00:00:00') AS DATETIME),
@@ -133,6 +131,76 @@ BEGIN
   CLOSE cursor_tp;
 
   COMMIT;
+
+
+  START TRANSACTION;
+
+  TRUNCATE TABLE icinga_sla_periods;
+
+  SELECT
+      CAST(DATE_FORMAT(DATE_SUB(NOW(), INTERVAL 4 YEAR), '%Y-01-01 00:00:00') AS DATETIME),
+      CAST(DATE_FORMAT(DATE_ADD(NOW(), INTERVAL 1 YEAR), '%Y-12-31 23:59:59') AS DATETIME)
+    INTO t_start, t_end;
+
+  OPEN cursor_tp;
+
+  tp_loop: LOOP
+    FETCH cursor_tp INTO tp_id, tpo_id;
+    IF done THEN
+      LEAVE tp_loop;
+    END IF;
+
+    SET @tp_lastend := NULL,
+        @tp_lastday := NULL,
+        @day_offset := NULL;
+
+    INSERT
+      INTO icinga_sla_periods SELECT
+        tpo_id,
+        DATE_ADD(CAST(monthly.date AS DATETIME), INTERVAL finaltps.start_sec SECOND) AS start_time,
+        DATE_ADD(CAST(monthly.date AS DATETIME), INTERVAL finaltps.end_sec SECOND) AS end_time
+
+      FROM (
+        SELECT
+          DATE_ADD(DATE(t_start), INTERVAL @day_offset := @day_offset + 1 DAY) AS date,
+          DAYOFWEEK(DATE_ADD(DATE(t_start), INTERVAL @day_offset DAY)) - 1 AS weekday
+
+        FROM icinga_objects o
+        JOIN (SELECT @day_offset := -1) day_offset
+
+        ORDER BY object_id
+        LIMIT 2194
+      ) monthly JOIN (
+        SELECT
+          NULL AS day,
+          NULL as start_sec,
+          NULL AS end_sec
+        FROM DUAL
+        WHERE (@tp_lastday := NULL) IS NOT NULL
+          AND ((@tp_lastend := 0) + (@day_offset := -1)) = 1
+
+        UNION
+
+          SELECT
+            day,
+            start_sec AS start_sec,
+            end_sec AS end_sec
+          FROM icinga_timeperiod_timeranges tpr
+
+          JOIN icinga_timeperiods tp ON tp.timeperiod_id = tpr.timeperiod_id
+          WHERE tp.timeperiod_object_id = tpo_id
+
+      ) finaltps ON finaltps.day = monthly.weekday
+      WHERE DATE_ADD(CAST(monthly.date AS DATETIME), INTERVAL finaltps.end_sec - 1 SECOND) <= t_end
+      ORDER BY monthly.date, finaltps.start_sec, finaltps.end_sec
+      ;
+
+  END LOOP tp_loop;
+
+  CLOSE cursor_tp;
+
+  COMMIT;
+
   SET SESSION binlog_format = STATEMENT;
   
   -- Workaround for nasty warning:
