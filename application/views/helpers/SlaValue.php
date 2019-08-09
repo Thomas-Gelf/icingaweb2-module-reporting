2bfc5cb582181b08b3b06589e5ce1817c135413a
Thomas Gelf <thomas@gelf.net>
2019-08-09 08:27:27 +0200

    SlaValue: allow to disable history links

--- application/views/helpers/SlaValue.php
+++ application/views/helpers/SlaValue.php
@@ -1,9 +1,12 @@
 <?php
 
+use Icinga\Application\Config;
 use Icinga\Web\Url;
 
 class Zend_View_Helper_SlaValue extends Zend_View_Helper_Abstract
 {
+    protected static $linkToHistory;
+
     /**
      * @param $value
      * @param $timeframe
@@ -27,13 +30,30 @@ class Zend_View_Helper_SlaValue extends Zend_View_Helper_Abstract
 
         $params->add('limit', 100);
 
-        return $this->view->qlink(
-            sprintf('%.3f %%', $value),
-            $url,
-            null,
-            array(
-               'title' => sprintf('%f', $value)
-            )
-        );
+        if ($this->linkToHistory()) {
+            return $this->view->qlink(
+                sprintf('%.3f %%', $value),
+                $url,
+                null,
+                ['title' => sprintf('%f', $value)]
+            );
+        } else {
+            return $this->view->qlink(
+                sprintf('%.3f %%', $value),
+                '#',
+                null,
+                ['title' => sprintf('%f', $value)]
+            );
+        }
+    }
+
+    protected static function linkToHistory()
+    {
+        if (self::$linkToHistory === null) {
+            self::$linkToHistory = Config::module('reporting')
+                ->get('ui', 'link_to_history', 'yes') === 'yes';
+        }
+
+        return self::$linkToHistory;
     }
 }
